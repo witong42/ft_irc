c665596adb1cdf17d5f9ea47a0543a57040a2515
JeanGIRARDREYDFET <cogitafac@gmail.com>
2026-01-26 10:55:24 +0100

    test du mot de passe

--- class/IrcCmdPass.cpp
+++ class/IrcCmdPass.cpp
@@ -11,12 +11,20 @@ bool Irc::CmdPass(std::vector<String> argument, Server &server)
 	(void)server;
 	if (!checkParams(argument.size(), 2, "PASS"))
 		return false;
-
 	if (_current_client->isRegistered())
 	{
 		_current_client->reply(ERR_ALREADYREGISTERED(_current_nick));
 		return false;
 	}
-
+	if (server.CheckPassword(argument[1], _current_client->getFd()))
+	{
+		_current_client->reply("001 " + _current_nick + " :Welcome to the IRC Network\r\n");
+		_current_client->setRegistered(true);
+	}
+	else
+	{
+		_current_client->reply("464 " + _current_nick + " :Password incorrect\r\n");
+		return false;
+	}
 	return true;
 }
