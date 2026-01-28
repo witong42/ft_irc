56a1887d9bec4fe18e402221bcd5fa69d55931f6
JeanGIRARDREYDFET <cogitafac@gmail.com>
2026-01-28 11:19:12 +0100

    // ajout de controle pour l'enregistrement des clients

--- class/IrcTryRegisterClient.cpp
+++ class/IrcTryRegisterClient.cpp
@@ -6,7 +6,7 @@
 /*   By: jegirard <jegirard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 11:03:04 by jegirard          #+#    #+#             */
-/*   Updated: 2026/01/28 11:07:10 by jegirard         ###   ########.fr       */
+/*   Updated: 2026/01/28 11:14:29 by jegirard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 
 bool Irc::TryRegisterClient(Client &client)
 {
+	if (client.isPwdOk() == false)
+		return false;
 	if (client.isRegistered())
 		return true;
-
 	if (client.getNickname().empty() || client.getUsername().empty())
 		return false;
-
 	client.reply(RPL_WELCOME(client.getNickname(), client.getUsername(), client.getHostname()) + "\r\n");
 	client.setRegistered(true);
 	return true;
